99e634e66ae0c18ae00b1faf390e4c8f2d04bd88
Marko Bozic <markobozic346@gmail.com>
2022-08-28 18:39:29 +0200

    Add utility for truncate user address

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -20,3 +20,11 @@ export const calculateRatio = (first: BigNumber, second: BigNumber) => {
 
   return parseInt(rawRatio.toString().split(".")[0]);
 };
+
+const truncateRegex = /^(0x[a-zA-Z0-9]{4})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/;
+
+export const truncateEthAddress = (address: string) => {
+  const match = address.match(truncateRegex);
+  if (!match) return address;
+  return `${match[1]}…${match[2]}`;
+};
